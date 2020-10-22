8584505f284ae0aafd7b8e6fe1a5442e0c00a70b
Julius Parulek <parulek@gmail.com>
2020-10-22 16:35:22 +0200

    Change obervations to standart error y plot

--- tests/plots/test_controller.py
+++ tests/plots/test_controller.py
@@ -18,19 +18,14 @@ def test_observation_plot_representation():
     x_axis = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
     plots = _get_observation_plots(observation_df, x_axis)
 
-    assert len(plots) == 3
+    assert len(plots) == 1
     assert "mode" in plots[0].repr
     for plot in plots:
         np.testing.assert_equal(observation_df["x_axis"].values, plot.repr.x)
         assert len(plot.repr.y) == len(observation_df)
 
     np.testing.assert_equal(plots[0].repr.y, observation_df["values"].values)
-    np.testing.assert_equal(
-        plots[1].repr.y, (observation_df["values"] - observation_df["std"]).values
-    )
-    np.testing.assert_equal(
-        plots[2].repr.y, (observation_df["values"] + observation_df["std"]).values
-    )
+    np.testing.assert_equal(plots[0].repr.error_y.array, observation_df["std"].values)
 
 
 def test_realizations_plot_representation():
