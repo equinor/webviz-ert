a7c094609e6067a3738da2f8f661f4668ba4fd6c
Åsmund Birkeland <musiv@equinor.com>
2020-10-26 16:10:39 +0100

    remove ert_client

--- ertviz/ert_client/__init__.py
+++
@@ -1,9 +0,0 @@
-from ertviz.models import Response, EnsembleModel
-
-
-def get_response(name, ref_url):
-    return Response(name, ref_url=ref_url)
-
-
-def get_ensemble(ref_url):
-    return EnsembleModel(ref_url=ref_url)
