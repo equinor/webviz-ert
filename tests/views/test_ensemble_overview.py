98da64579d849b092b147e73e8e58bf26e4df71a
Øystein Olai Heggen <ooh@equinor.com>
2020-11-12 15:10:08 +0100

    Add test to view ensemble_overview

--- tests/views/test_ensemble_overview.py
+++ tests/views/test_ensemble_overview.py
@@ -1,12 +1,27 @@
 import dash
-import dash_html_components as html
-import pandas as pd
-from ertviz.plugins._ensemble_overview import EnsembleOverview
 
-def test_ensemble_overview(dash_duo, monkeypatch, mocker, tmpdir):
+import ertviz
+from ertviz.plugins._ensemble_overview import EnsembleOverview
+from tests.data.snake_oil_data import ensembles_response
+
+
+def test_ensemble_overview(dash_duo, monkeypatch, mocker):
     app = dash.Dash(__name__)
+
+    mock_data = ensembles_response["http://127.0.0.1:5000/ensembles"]["ensembles"]
+
+    monkeypatch.setattr(
+        ertviz.plugins._ensemble_overview,
+        "get_ensembles",
+        mocker.Mock(return_value=mock_data),
+    )
 
     plugin = EnsembleOverview(mocker.Mock())
     layout = plugin.layout
     app.layout = layout
     dash_duo.start_server(app)
+
+    ensemble_elements = dash_duo.find_elements("a")
+    assert ensemble_elements[0].text == "default"
+    assert ensemble_elements[1].text == "default_smoother_update"
+    assert dash_duo.get_logs() == [], "browser console should contain no error"
