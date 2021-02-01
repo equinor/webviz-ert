82f8977980d9f33f69bbc59071d3b83339824498
Åsmund Birkeland <aasmund.birkeland@gmail.com>
2021-02-01 11:39:01 +0100

    Remove outdated plugin

--- ertviz/plugins/__init__.py
+++ ertviz/plugins/__init__.py
@@ -1,4 +1,3 @@
 from ._ensemble_overview import EnsembleOverview
-from ._ensemble_comparison import EnsembleComparison
 from ._response_comparison import ResponseComparison
 from ._observation_analyzer import ObservationAnalyzer
