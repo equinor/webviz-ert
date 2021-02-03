4ee9a9ef4045cee5e27d2d182afcab48062ece95
Anette Uttisrud <autt@equinor.com>
2021-02-03 10:39:37 +0100

    Fix typo in filename

--- ertviz/views/__init__.py
+++ ertviz/views/__init__.py
@@ -1,4 +1,4 @@
-from .paremeter_view import parameter_view
+from .parameter_view import parameter_view
 from .ensemble_selector_view import ensemble_selector_view
 from .response_view import response_view
 from .plot_view import plot_view_body, plot_view_header
