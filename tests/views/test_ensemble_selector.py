c40512f4db6fb18581a76e51a7226709cd671344
DanSava <dan.sava42@gmail.com>
2022-05-30 18:37:56 +0300

    Show ensemble color for each selected ensemble

--- tests/views/test_ensemble_selector.py
+++ tests/views/test_ensemble_selector.py
@@ -1,5 +1,5 @@
 import dash
-
+from webviz_ert.assets import get_color
 from webviz_ert.plugins import ParameterComparison
 from tests.conftest import select_first, get_options
 
@@ -83,3 +83,33 @@ def test_ensemble_refresh(
         dash_duo, "#" + plugin.uuid("ensemble-multi-selector")
     )
     assert refreshed_cases == expected_cases
+
+
+def test_ensemble_color(mock_data, dash_duo):
+    app = dash.Dash(__name__)
+    plugin = ParameterComparison(app, project_identifier=None)
+    app.layout = plugin.layout
+    dash_duo.start_server(app)
+
+    windowsize = (630, 1200)
+    dash_duo.driver.set_window_size(*windowsize)
+
+    ensembles = get_options(dash_duo, "#" + plugin.uuid("ensemble-multi-selector"))
+
+    for _ in ensembles:
+        ensemble_name = select_first(
+            dash_duo, "#" + plugin.uuid("ensemble-multi-selector")
+        )
+        dash_duo.wait_for_contains_text(
+            "#" + plugin.uuid("selected-ensemble-dropdown"),
+            ensemble_name,
+            timeout=4,
+        )
+
+    selected_ensembles = dash_duo.find_elements(
+        "#" + plugin.uuid("selected-ensemble-dropdown") + " div div.Select-value"
+    )
+    for idx, ensemble in enumerate(selected_ensembles):
+        color = ensemble.value_of_css_property("background-color")
+        expected_color = get_color(idx)
+        assert color.replace(" ", "") == expected_color
