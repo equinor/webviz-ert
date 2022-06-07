ddd39ce487de1f31debd932fbf7ee1a24a308fbf
DanSava <dan.sava42@gmail.com>
2022-06-07 15:24:33 +0300

    Add help information for pages

--- webviz_ert/plugins/_response_comparison.py
+++ webviz_ert/plugins/_response_comparison.py
@@ -1,5 +1,6 @@
 import dash
 from dash.development.base_component import Component
+from typing import List, Dict
 
 import webviz_ert.models
 from dash import html
@@ -39,6 +40,74 @@ class ResponseComparison(WebvizErtPluginABC):
                 ),
             ]
         )
+
+    @property
+    def tour_steps(self) -> List[Dict[str, str]]:
+        steps = [
+            {
+                "id": self.uuid("ensemble-multi-selector"),
+                "content": "List of experiment ensembles.",
+            },
+            {
+                "id": self.uuid("selected-ensemble-dropdown"),
+                "content": "List of currently selected ensembles.",
+            },
+            {
+                "id": self.uuid(f"ensemble-refresh-button"),
+                "content": (
+                    "Forces a refresh of all ensemble data including parameter and response data. \n"
+                ),
+            },
+            {
+                "id": self.uuid("response-section"),
+                "content": "Response section.",
+            },
+            {
+                "id": self.uuid(f"parameter-selector-multi-resp"),
+                "content": (
+                    "List of responses. This list is populated only"
+                    " if at least one ensemble is selected."
+                    " Selecting multiple responses is possible"
+                    " using mouse `Click + Drag` inside the response list."
+                ),
+            },
+            {
+                "id": self.uuid(f"parameter-deactivator-resp"),
+                "content": "List of currently selected responses.",
+            },
+            {
+                "id": self.uuid(f"parameter-selector-filter-resp"),
+                "content": (
+                    "Response search field. The response list will show only"
+                    " elements that contain the search characters"
+                ),
+            },
+            {
+                "id": self.uuid("parameter-section"),
+                "content": "Parameter section",
+            },
+            {
+                "id": self.uuid(f"parameter-selector-multi-param"),
+                "content": (
+                    "List of parameters. This list is populated only"
+                    " if at least one ensemble is selected."
+                    " Selecting multiple parameters is possible"
+                    " using mouse `Click + Drag` inside the response list."
+                ),
+            },
+            {
+                "id": self.uuid(f"parameter-deactivator-param"),
+                "content": "List of currently selected parameters.",
+            },
+            {
+                "id": self.uuid(f"parameter-selector-filter-param"),
+                "content": (
+                    "Search field. The parameter list will show only"
+                    " elements that contain the search characters"
+                ),
+            },
+        ]
+        return steps
 
     def set_callbacks(self, app: dash.Dash) -> None:
         webviz_ert.controllers.ensemble_list_selector_controller(self, app)
