d7e02db7aa088e593a9bdd82c0df8609619b112e
hnformentin <helena.formentin@gmail.com>
2022-07-18 15:30:04 +0200

    Refactor, extract _update_corr_index_dict and add tests

--- tests/controllers/test_response_correlation_controller.py
+++ tests/controllers/test_response_correlation_controller.py
@@ -1,4 +1,10 @@
 import pandas as pd
+import pytest
+from webviz_ert.controllers.response_correlation_controller import (
+    _get_first_observation_x,
+    _define_style_ensemble,
+    _update_corr_index_dict,
+)
 
 from webviz_ert.controllers.response_correlation_controller import (
     sort_dataframe,
@@ -16,3 +22,78 @@ def test_sort_dataframe():
     sorted_dataframe, _ = sort_dataframe(dataframe, index, other_key)
     assert list(sorted_dataframe[other_key]) == sorted(data[other_key])
     assert list(sorted_dataframe[one_key]) == [0.2, -0.4, 0.6]
+
+
+@pytest.mark.parametrize(
+    "index,expected_color", [(0, "rgba(56,108,176,0.8)"), (1, "rgba(127,201,127,0.8)")]
+)
+def test_define_style_ensemble_color(index, expected_color):
+    x_axis = pd.DataFrame([1])
+    style = _define_style_ensemble(index, x_axis)
+    assert style["line"]["color"] == expected_color
+    assert style["marker"]["color"] == expected_color
+
+
+@pytest.mark.parametrize(
+    "x_axis_list,expected_mode",
+    [
+        (
+            [pd._libs.tslibs.timestamps.Timestamp("01-01-2020")],
+            "markers+lines",
+        ),
+        ([str(1)], "markers"),
+    ],
+)
+def test_define_style_ensemble_mode(x_axis_list, expected_mode):
+    x_axis = pd.Index(x_axis_list)
+    style = _define_style_ensemble(0, x_axis)
+    assert style["mode"] == expected_mode
+
+
+@pytest.mark.parametrize(
+    "response_x_axis,observation_x_axis_list,expected_index",
+    [
+        (
+            [
+                "2020-01-01 00:00:00",
+                "2020-01-07 00:00:00",
+                "2020-01-14 00:00:00",
+                "2020-01-21 00:00:00",
+            ],
+            [
+                pd._libs.tslibs.timestamps.Timestamp("14-01-2020"),
+                pd._libs.tslibs.timestamps.Timestamp("01-02-2020"),
+            ],
+            2,
+        ),
+        (pd.Index([0, 1]), [str(1)], 1),
+    ],
+)
+def test_update_corr_index_dict(
+    response_x_axis, observation_x_axis_list, expected_index
+):
+    observation_x_axis = pd.DataFrame(observation_x_axis_list, columns=["x_axis"])
+    updated_index = _update_corr_index_dict(response_x_axis, observation_x_axis)
+    assert expected_index == updated_index
+
+
+@pytest.mark.parametrize(
+    "observation,expected",
+    [
+        ([str(1)], int(1)),
+        (
+            [pd._libs.tslibs.timestamps.Timestamp("01-01-2020")],
+            str("2020-01-01 00:00:00"),
+        ),
+    ],
+)
+def test_get_first_observation_x_valid(observation, expected):
+    df_observation = pd.DataFrame(observation, columns=["x_axis"])
+    result = _get_first_observation_x(df_observation)
+    assert result == expected
+
+
+def test_get_first_observation_x_invalid():
+    df_observation = pd.DataFrame([int(1)], columns=["x_axis"])
+    with pytest.raises(ValueError, match="invalid obs_data type"):
+        _get_first_observation_x(df_observation)
