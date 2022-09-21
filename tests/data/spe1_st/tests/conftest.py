2b06b83b85e0da12044e217e61eaa883010ccaee
Eivind Jahren <ejah@equinor.com>
2022-09-21 11:38:04 +0200

    Update location of ert services

--- tests/data/spe1_st/tests/conftest.py
+++ tests/data/spe1_st/tests/conftest.py
@@ -3,7 +3,7 @@ import pytest
 
 @pytest.fixture(scope="session", autouse=True)
 def start_storage_server():
-    from ert.shared.services import Storage
+    from ert.services import Storage
 
     with Storage.start_server() as service:
         service.wait_until_ready(timeout=30)
