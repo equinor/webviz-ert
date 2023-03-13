c7feb8c3f106cf3c915597bc5daa689ea5b6c967
Øyvind Eide <oyveid@equinor.com>
2023-03-13 13:16:51 +0100

    Update to new ert api for StorageService

--- tests/data/spe1_st/tests/conftest.py
+++ tests/data/spe1_st/tests/conftest.py
@@ -3,8 +3,8 @@ import pytest
 
 @pytest.fixture(scope="session", autouse=True)
 def start_storage_server():
-    from ert.services import Storage
+    from ert.services import StorageService
 
-    with Storage.start_server() as service:
+    with StorageService.start_server() as service:
         service.wait_until_ready(timeout=30)
         yield service
