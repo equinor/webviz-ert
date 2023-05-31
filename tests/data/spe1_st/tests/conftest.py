2495f712502714eb6f4a3eb0fb2a770bd3ded50c
Matt Hall <mtha@equinor.com>
2023-05-31 15:45:36 +0200

    Allow for ert4 or ert5 storage service

--- tests/data/spe1_st/tests/conftest.py
+++ tests/data/spe1_st/tests/conftest.py
@@ -3,8 +3,11 @@ import pytest
 
 @pytest.fixture(scope="session", autouse=True)
 def start_storage_server():
-    from ert.services import StorageService
+    try:
+        from ert.services import StorageService as Storage
+    except ImportError:
+        from ert.services import Storage
 
-    with StorageService.start_server() as service:
+    with Storage.start_server() as service:
         service.wait_until_ready(timeout=30)
         yield service
