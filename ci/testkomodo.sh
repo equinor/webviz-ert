075d093dd872b066c3f8ea652176aa17730b650d
larsevj <levje@equinor.com>
2024-01-19 12:41:42 +0100

    remove unused function install_package

--- ci/testkomodo.sh
+++ ci/testkomodo.sh
@@ -3,10 +3,6 @@ copy_test_files () {
     cp -r $CI_SOURCE_ROOT/tests $CI_TEST_ROOT/tests
 }
 
-install_package () {
-    pip install . --no-deps
-}
-
 start_tests () {
     start_integration_test
     test_result=$?
