633494816f5586322c08f4e816fad7e80b30d964
DanSava <dan.sava42@gmail.com>
2024-08-08 10:46:30 +0300

    Handle ensemble schema without parent and child ensemble_ids

--- tests/models/test_ensemble_model.py
+++ tests/models/test_ensemble_model.py
@@ -7,8 +7,6 @@ from webviz_ert.models import load_ensemble
 
 def test_ensemble_model(mock_data):
     ens_model = EnsembleModel(ensemble_id=1, project_id=None)
-    assert len(ens_model.children) == 1
-    assert ens_model.children[0].name == "default_smoother_update"
     assert ens_model.name == "default"
     assert len(ens_model.responses) == 1
 
